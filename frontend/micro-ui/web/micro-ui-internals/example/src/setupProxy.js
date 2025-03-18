f8a95f4c34559cdec0c31bdcd849f94bd9f5135f
umeshkumar <ukm.office3423@gmail.com>
2025-03-18 18:16:26 +0530

    first

--- frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const createProxy = createProxyMiddleware({
   //target: process.env.REACT_APP_PROXY_API || "https://uat.digit.org",
-  // target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
-  target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
+  // target: process.env.REACT_APP_PROXY_API || "https://digit.sparrowsoftech.in",
+  target: process.env.REACT_APP_PROXY_API || "https://digit.sparrowsoftech.in",
   changeOrigin: true,
 });
 const assetsProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_ASSETS || "https://qa.digit.org",
+  target: process.env.REACT_APP_PROXY_ASSETS || "https://digit.sparrowsoftech.in",
   changeOrigin: true,
 });
 module.exports = function (app) {
